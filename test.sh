6a55185e0e753580f3b5aa0572da1d4c23db537c
TheArchSeal <simon.fikse0414@gmail.com>
2025-05-13 22:30:00 +0200

    made negative testing possible

--- test.sh
+++ test.sh
@@ -4,16 +4,16 @@ passed=0
 total=0
 
 for program in bin/tests/*; do
-    [ -x $program ] || continue
+    [ -x "$program" ] || continue
     category=$(basename "$program")
 
     for input in tests/"$category"/cases/*.sml; do
-        [ -r $input ] || continue
+        [ -r "$input" ] || continue
         case=$(basename "$input");
-        output=${input%.sml}.out
 
-        res=$("$program" "$input" 2>&1) && res=$(echo "$res"| diff "$output" -)
-        if [ $? -eq 0 ]; then
+        res=$("$program" "$input" 2>&1) && output="${input%.sml}.out" || output="${input%.sml}.err"
+        [ -r "$output" ] && res=$(echo "$res" | diff "$output" -)
+        if [ $? = 0 ]; then
             printf "$category/$case: passed\n"
             ((passed++))
         else
